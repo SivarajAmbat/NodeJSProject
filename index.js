4c91a06ac58d9c500f619fd2abfc0e572cc689d5
Sivaraj TechM <sivaraj.ambat@techmahindra.com>
2020-02-11 03:51:54 +0000

    V1

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+res.sendFile(`${__dirname}/index.html`)
+})
+
+app.listen(3000, () => {
+console.log('V1: Listening on port 3000')
+})
